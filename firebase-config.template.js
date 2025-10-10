576077b6444a900f161ce418d53ccd54137df777
LAFFER-BIT <laffersun@fnfcorp.com>
2025-10-10 17:00:55 +0800

    Create firebase-config.template.js

---
+++ firebase-config.template.js
@@ -0,0 +1,9 @@
+// 占位模板，运行时会被 CI 生成的文件取代
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "__API_KEY__",
+  authDomain: "__AUTH_DOMAIN__",
+  projectId: "__PROJECT_ID__",
+  storageBucket: "__STORAGE_BUCKET__",
+  messagingSenderId: "__MSG_SENDER_ID__",
+  appId: "__APP_ID__"
+};
