1d73b29b81029d01550e7993d5745f1c9fd83682
LAFFER-BIT <LAFFER-BIT@users.noreply.github.com>
2025-10-11 03:26:37 +0000

    deploy: 9d06814f65bf01f185fcb0d18033e7beb6b0abce

---
+++ config.js
@@ -0,0 +1,13 @@
+// 占位模板，运行时会被 CI 生成的文件取代
+window.APP_CONFIG = {
+  production: {
+    firebase: {
+      apiKey: "__API_KEY__",
+      authDomain: "fitness-management4p.firebaseapp.com",            // 例如 your-project.firebaseapp.com
+      projectId: "__PROJECT_ID__",
+      storageBucket: "fitness-management4p.firebasestorage.app", // ✅ 正确的存储桶域名
+      messagingSenderId: "__MSG_SENDER_ID__",
+      appId: "__APP_ID__"
+    }
+  }
+};
