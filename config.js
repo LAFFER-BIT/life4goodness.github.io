2c20aa6e9b99e9e3a7ae0d931bc72614dc399bc8
LAFFER-BIT <laffersun@fnfcorp.com>
2025-10-11 09:07:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ window.APP_CONFIG = {
   production: {
     firebase: {
       apiKey: "__API_KEY__",
-      authDomain: "__AUTH_DOMAIN__",            // 例如 your-project.firebaseapp.com
+      authDomain: "fitness-management4p.firebaseapp.com",            // 例如 your-project.firebaseapp.com
       projectId: "__PROJECT_ID__",
-      storageBucket: "__PROJECT_ID__.appspot.com", // ✅ 正确的存储桶域名
+      storageBucket: "fitness-management4p.firebasestorage.app", // ✅ 正确的存储桶域名
       messagingSenderId: "__MSG_SENDER_ID__",
       appId: "__APP_ID__"
     }
