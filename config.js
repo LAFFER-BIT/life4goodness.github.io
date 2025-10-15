66c224cc7f7272a58f36dc4bd1ce6efaa0382662
LAFFER-BIT <laffersun@fnfcorp.com>
2025-10-15 17:14:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,10 @@
 // 占位模板，运行时会被 CI 生成的文件取代
 window.APP_CONFIG = {
   production: {
-    firebase: {
-        apiKey: "AIzaSyDmLT56NhPS7waFqssx9YPlqkB-eGFbOQw",
-        projectId: "fitness-management4p",
-        appId: "1:752371346113:web:9abbf22bddba5e77364240",
-        enabled: true
-
-    },
     leancloud: {
-        appId: "U5uEmTjolCTyItwSg6ALj9AV-gzGzoHsz",
-        appKey: "dCcxXHLYUwAUSu7S7KVBEMuz",
-        serverURL: "https://u5uemtjo.lc-cn-n1-shared.com",
-        enabled: true
-    },
-    defaultSync: 'auto'
+      appId: "__APP_ID__",           // LeanCloud AppID
+      appKey: "__APP_KEY__",         // LeanCloud AppKey
+      serverURL: "__SERVER_URL__"    // 例如: https://your-app.lc-cn-n1-shared.com
     }
+  }
 };
